f507bb4092db94f2f2161b64773088b056053814
MrRiles9 <9rileys@gmail.com>
2025-03-19 12:33:30 -0700

    added player ship, movement funcionality and asteroid field

--- main.py
+++ main.py
@@ -1,24 +1,53 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-	pygame.init()
+    pygame.init()
 
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	print("Starting Asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
-	running = True
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-		screen.fill((0, 0, 0))
-		pygame.display.flip()
+    Asteroid.containers = (asteroids, updateable, drawable)
 
-	pygame.quit()
+    AsteroidField.containers = (updateable,)
+
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updateable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    
+
+    clock = pygame.time.Clock()
+    dt = 0  
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        dt = clock.tick(60) / 1000
+    
+        updateable.update(dt)
+        
+        screen.fill((0, 0, 0))
+        
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
+
+        pygame.display.flip()
+
+    pygame.quit()
 
 if __name__ == "__main__":
-	main()
+    main()
